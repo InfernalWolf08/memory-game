f456f7948e191ad6323e00530b4ef4620aa38a6f
InfernalWolf08 <jayinjoshi@gmail.com>
2025-07-17 16:59:36 -0700

    New Project

---
+++ Assets/Scripts/PlayerDraw.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDraw : MonoBehaviour
+{
+    [Header("Mouse Data")]
+    private Camera cam;
+    private CursorController cursor;
+
+    [Header("Color Data")]
+    public Color32 selectedColor = Color.white;
+    public List<Color32> canvas = new List<Color32>();
+
+    void Start()
+    {
+        // Initialize
+        cam = GetComponent<Camera>();
+        cursor = GetComponent<CursorController>();
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // Hotkeys
+        if (Input.GetKeyDown("1"))
+        {
+            selectedColor = new Color32(255, 0, 0, 255); // Red
+        } else if (Input.GetKeyDown("2")) {
+            selectedColor = new Color32(255, Convert.ToByte(127.5), 0, 255); // Orange
+        } else if (Input.GetKeyDown("3")) {
+            selectedColor = new Color32(255, 255, 0, 255); // Yellow
+        } else if (Input.GetKeyDown("4")) {
+            selectedColor = new Color32(0, 255, 0, 255); // Green
+        } else if (Input.GetKeyDown("5")) {
+            selectedColor = new Color32(0, 0, 255, 255); // Blue
+        } else if (Input.GetKeyDown("6")) {
+            selectedColor = new Color32(Convert.ToByte(127.5), 0, 255, 255); // Purple
+        } else if (Input.GetKeyDown("7")) {
+            selectedColor = new Color32(255, 0, 255, 255); // Pink
+        } else if (Input.GetKeyDown("8")) {
+            selectedColor = new Color32(Convert.ToByte(127.5), Convert.ToByte(63.75), 0, 255); // Brown
+        } else if (Input.GetKeyDown("9")) {
+            selectedColor = new Color32(0, 0, 0, 255); // Black
+        } else if (Input.GetKeyDown("0")) {
+            selectedColor = new Color32(255, 255, 255, 255); // White
+        }
+
+        // Paint
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+            
+            // Change tile color
+            try
+            {
+                if (hit.transform.gameObject.tag=="Tile")
+                {
+                    hit.transform.gameObject.GetComponent<SpriteRenderer>().color = selectedColor;
+                }
+            } catch (Exception e) {
+                print("No tile was hit");
+            }
+        }
+    }
+
+    // Custom functions
+    public void changeColor(GameObject button)
+    {
+        selectedColor = button.GetComponent<Image>().color;
+        // cursor.setCursorColor(selectedColor);
+    }
+}
---
+++ Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    [Header("Settings")]
+    public Vector2 startPoint;
+    public GameObject tile;
+    public Vector2 gridSize;
+    public float offset;
+
+    [Header("Data")]
+    public GameObject[] tiles;
+    private int amount;
+
+    void Start()
+    {
+        // Initialize
+        amount = 1;
+        tiles = generate(startPoint, tile, gridSize, offset);
+    }
+
+    // Custom functions
+    public GameObject[] generate(Vector2 startPoint, GameObject tile, Vector2 gridSize, float offset)
+    {
+        // Generate tiles
+        for (int y=0; y<gridSize.y; y++)
+        {
+            for (int x=0; x<gridSize.x; x++)
+            {
+                GameObject clone = Instantiate(tile, new Vector3(startPoint.x+(offset*x), startPoint.y+(offset*y), 20), Quaternion.identity, transform);
+                clone.name += $" ({amount})";
+                amount += 1;
+            }
+        }
+
+        // Put tiles into an array
+        int tileAmount = transform.childCount;
+        GameObject[] tiles = new GameObject[tileAmount];
+
+        for (int i=0; i<tileAmount; i++)
+        {
+            tiles[i] = transform.GetChild(i).gameObject;
+        }
+
+        return tiles;
+    }
+}
