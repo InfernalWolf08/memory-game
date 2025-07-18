c0493444bb9a43bd1a835131d834a1ba9d9c12af
InfernalWolf08 <jayinjoshi@gmail.com>
2025-07-18 11:24:51 -0700

    Made images configurable

--- Assets/Scripts/ImageController.cs
+++ Assets/Scripts/ImageController.cs
@@ -7,9 +7,12 @@ public class ImageController : MonoBehaviour
     [Header("Image Data")]
     public List<ImageArray> images = new List<ImageArray>();
 
+    // Custom functions
     public void ImportImage(List<Color32> colors)
     {
-        
+        ImageArray image = new ImageArray();
+        image.imageColors = colors.ToArray();
+        images.Add(image);
     }
 }
 
